38b3c213ba420a788bc2cb01d911d3466c50f74a
Luis G. López <lopezsolerluis@gmail.com>
2021-09-21 13:53:56 -0300

    Pequeña errata en la disposición de las leyendas

--- src/lopezsolerluis/ecuacion_de_tiempo.cljs
+++ src/lopezsolerluis/ecuacion_de_tiempo.cljs
@@ -94,7 +94,7 @@
      [:> rvis/XAxis {:tickSizeInner 0 :tickSizeOuter 6 :style axis-style :tickFormat #(ecu/ms->mes %)}]
      [:> rvis/YAxis {:tickSizeInner 0 :tickSizeOuter 6 :style axis-style :tickFormat  #(ecu/ms->hms %)}]
      [:> rvis/DiscreteColorLegend {:style {:position "absolute" :left 120 :top 10}
-                                   :orientation "horizontal"
+                                   :orientation "vertical"
                                    :items [{:title " Ecuación de Tiempo" :color color1 :strokeWidth 3}
                                            {:title " Reducción al Ecuador" :color color3 :strokeWidth 3}
                                            {:title " Ecuación de Centro"  :color color2 :strokeWidth 3}]}]
